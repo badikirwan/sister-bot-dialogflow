c8bb7ef62216f8cc55ab3012da729f843aee42f0
badikirwan <badikirwan@gmail.com>
2019-06-29 06:04:27 +0700

    update app.js

--- app.js
+++ app.js
@@ -15,10 +15,6 @@ server.use(bodyParser.json());
 server.post('/webhook', function (req, res) {
 
     if(req.body.queryResult.intent.displayName == "LihatNilaiAkademik") {
-        res.send(JSON.stringify({
-            "fulfillmentText" : "Baik, tolong inputkan NIM dan semester anda \n Contoh : Nim 201569040006 semester 7",
-        }));
-
         if(req.body.queryResult.intent.displayName == "LihatNilaiAkademik") {
             if(req.body.queryResult.action == "LihatNilaiAkademik.LihatNilaiAkademik-custom" && req.body.queryResult.parameters.nim != "" 
                 && req.body.queryResult.parameters.semester != "") {
