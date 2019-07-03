eec5a2c09a3f3f6e2197cc9bac899f91219ba0c6
badikirwan <badikirwan@gmail.com>
2019-07-03 12:46:11 +0700

    update app.js

--- app.js
+++ app.js
@@ -13,154 +13,51 @@ const server = express();
 
 server.use(bodyParser.json());
 server.post('/webhook', function (req, res) {
-
     if(req.body.queryResult.intent.displayName == "LihatNilaiAkademik") {
-        if(req.body.queryResult.intent.displayName == "LihatNilaiAkademik") {
-            if(req.body.queryResult.action == "LihatNilaiAkademik.LihatNilaiAkademik-custom" && req.body.queryResult.parameters.nim != null 
-                && req.body.queryResult.parameters.semester != null) {
-                    var request = unirest("GET", "https://api.themoviedb.org/3/movie/top_rated");
-                        request.query({
-                            "page": "1",
-                            "language": "en-US",
-                            "api_key": "33a4f2f91284c9133695dfba6bd802da"
-                        });
-                        request.send("{}");
-                        request.end(function(response) {
-                            if(response.error) {
-                                res.setHeader('Content-Type', 'application/json');
-                                res.send(JSON.stringify({
-                                    "speech" : "Error. Can you try it again ? ",
-                                    "displayText" : "Error. Can you try it again ? "
-                                }));
-                            } else if(response.body.results.length > 0) {
-                                let result = response.body.results;
-                                let output = '';
-                                for(let i = 0; i<result.length;i++) {
-                                    output += result[i].title;
-                                    output+="\n"
-                                }
-                                res.setHeader('Content-Type', 'application/json');
-                                res.send(JSON.stringify({
-                                    "speech" : output,
-                                    "displayText" : output
-                                })); 
+        if(req.body.queryResult.action == "LihatNilaiAkademik.LihatNilaiAkademik-custom" && req.body.queryResult.parameters.nim != null 
+            && req.body.queryResult.parameters.semester != null) {
+                var request = unirest("GET", "https://api.themoviedb.org/3/movie/top_rated");
+                    request.query({
+                        "page": "1",
+                        "language": "en-US",
+                        "api_key": "33a4f2f91284c9133695dfba6bd802da"
+                    });
+                    request.send("{}");
+                    request.end(function(response) {
+                        if(response.error) {
+                            res.setHeader('Content-Type', 'application/json');
+                            res.send(JSON.stringify({
+                                "speech" : "Error. Can you try it again ? ",
+                                "displayText" : "Error. Can you try it again ? "
+                            }));
+                        } else if(response.body.results.length > 0) {
+                            let result = response.body.results;
+                            let output = '';
+                            for(let i = 0; i<result.length;i++) {
+                                output += result[i].title;
+                                output+="\n"
                             }
-                        });
-                    
-                }
+                            res.setHeader('Content-Type', 'application/json');
+                            res.send(JSON.stringify({
+                                "speech" : output,
+                                "displayText" : output
+                            })); 
+                        }
+                    });
+                
+            }
+    } else if(req.body.queryResult.intent.displayName == "LihatKalenderAkademik") {
+        if(req.body.queryResult.action == "LihatKalenderAkademik" && req.body.queryResult.parameters.tahun != null) {
+            res.send(JSON.stringify({
+                "speech" : "Error. Can you try it tahun ? ",
+                "displayText" : "Error. Can you try it tahun ? "
+            }));                
         }
+
+    } else if(req.body.queryResult.intent.displayName == ""){
+
     }
 
-    // if(req.body.queryResult.intent.displayName == "LihatKalenderAkademik") {
-    //     res.send(JSON.stringify({
-    //         "speech" : "Couldn't find any deatails. :(  ",
-    //         "displayText" : "Couldn't find any deatails. :(  "
-    //     }));
-    // }
-
-    // if(req.body.queryResult.intent.displayName == "LihatNilaiAkademik") {
-    //     res.send(JSON.stringify({
-    //         "speech" : "Couldn't find any deatails. :(  ",
-    //         "displayText" : "Couldn't find any deatails. :(  "
-    //     }));
-    // }
-
-    // if(request.body.queryResult.parameters['top-rated']) {
-    //     var req = unirest("GET", "https://api.themoviedb.org/3/movie/top_rated");
-    //         req.query({
-    //             "page": "1",
-    //             "language": "en-US",
-    //             "api_key": "33a4f2f91284c9133695dfba6bd802da"
-    //         });
-    //         req.send("{}");
-    //         req.end(function(res) {
-    //             if(res.error) {
-    //                 response.setHeader('Content-Type', 'application/json');
-    //                 response.send(JSON.stringify({
-    //                     "speech" : "Error. Can you try it again ? ",
-    //                     "displayText" : "Error. Can you try it again ? "
-    //                 }));
-    //             } else if(res.body.results.length > 0) {
-    //                 let result = res.body.results;
-    //                 let output = '';
-    //                 for(let i = 0; i<result.length;i++) {
-    //                     output += result[i].title;
-    //                     output+="\n"
-    //                 }
-    //                 response.setHeader('Content-Type', 'application/json');
-    //                 response.send(JSON.stringify({
-    //                     "speech" : output,
-    //                     "displayText" : output
-    //                 })); 
-    //             }
-    //         });
-    // } else if(request.body.result.parameters['movie-name']) {
-    //  //   console.log('popular-movies param found');
-    //     let movie = request.body.result.parameters['movie-name'];
-    //     var req = unirest("GET", "https://api.themoviedb.org/3/search/movie");
-    //         req.query({
-    //             "include_adult": "false",
-    //             "page": "1",
-    //             "query":movie,
-    //             "language": "en-US",
-    //             "api_key": "33a4f2f91284c9133695dfba6bd802da"
-    //         });
-    //         req.send("{}");
-    //         req.end(function(res) {
-    //             if(res.error) {
-    //                 response.setHeader('Content-Type', 'application/json');
-    //                 response.send(JSON.stringify({
-    //                     "speech" : "Error. Can you try it again ? ",
-    //                     "displayText" : "Error. Can you try it again ? "
-    //                 }));
-    //             } else if(res.body.results.length > 0) {
-    //             let result = res.body.results[0];
-    //             let output = "Average Rating : " + result.vote_average + 
-    //             "\n Plot : " + result.overview + "url" + result.poster_path
-    //                 response.setHeader('Content-Type', 'application/json');
-    //                 response.send(JSON.stringify({
-    //                     "speech" : output,
-    //                     "displayText" : output
-    //                 }));
-    //             } else {
-    //                 response.setHeader('Content-Type', 'application/json');
-    //                 response.send(JSON.stringify({
-    //                     "speech" : "Couldn't find any deatails. :(  ",
-    //                     "displayText" : "Couldn't find any deatails. :(  "
-    //                 }));
-    //             }
-    //         });
-
-    // } else if(request.body.result.parameters['popular-movies']) {    
-    //     var req = unirest("GET", "https://api.themoviedb.org/3/movie/popular");
-    //         req.query({
-    //             "page": "1",
-    //             "language": "en-US",
-    //             "api_key": "33a4f2f91284c9133695dfba6bd802da"
-    //         });
-    //         req.send("{}");
-    //         req.end(function(res){
-    //             if(res.error) {
-    //                 response.setHeader('Content-Type', 'application/json');
-    //                 response.send(JSON.stringify({
-    //                     "speech" : "Error. Can you try it again ? ",
-    //                     "displayText" : "Error. Can you try it again ? "
-    //                 }));
-    //             } else {
-    //                 let result = res.body.results;
-    //                 let output = '';
-    //                 for(let i = 0; i < result.length;i++) {
-    //                     output += result[i].title;
-    //                     output+="\n"
-    //                 }
-    //                 response.setHeader('Content-Type', 'application/json');
-    //                 response.send(JSON.stringify({
-    //                     "speech" : output,
-    //                     "displayText" : output
-    //                 })); 
-    //             }
-    //         });
-    // }
 });
 
 function sendMessage() {
